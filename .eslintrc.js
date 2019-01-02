baf471149fba7f935fb1e3db9dd8bf4d082cb903
Josh Justice <me@codingitwrong.com>
2019-01-02 06:42:54 -0600

    Configure eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    'codingitwrong',
+    'plugin:prettier/recommended',
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+};
